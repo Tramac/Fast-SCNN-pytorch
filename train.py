39c22c430ec849fced2216578870427b1da0121b
Tramac <1145955920@qq.com>
2019-03-29 14:36:18 +0800

    update train.py

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ def parse_args():
                         help='Auxiliary loss')
     parser.add_argument('--aux-weight', type=float, default=0.4,
                         help='auxiliary loss weight')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+    parser.add_argument('--epochs', type=int, default=160, metavar='N',
                         help='number of epochs to train (default: 100)')
     parser.add_argument('--start_epoch', type=int, default=0,
                         metavar='N', help='start epochs (default:0)')
